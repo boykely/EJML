253e0d463fbece71360dc28e3502f869c4fe8d43
boykely <valimo_ral@yahoo.fr>
2016-04-12 10:05:14 +0200

    + model A de brady
    fichier test: step2_1104_soir

---
+++ src/Fonction.java
@@ -0,0 +1,321 @@
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.simple.SimpleMatrix;
+
+public class Fonction implements LevenbergMarquardt.Function 
+{	
+	private Color lightColor;
+	public DenseMatrix64F oneChannelMData;	
+	public int compte=0;
+	public DenseMatrix64F Y;
+	private HashMap<Integer, int[]>p;
+	private int[] cam;
+	//lafortune 
+	private int[] lum;
+	@Override
+	public void compute(DenseMatrix64F param, DenseMatrix64F x, DenseMatrix64F y) 
+	{
+		// TODO Auto-generated method stub
+		//y=a*x^2+b*x+c
+		//A ne pas supprimer parce que utiliser pour des test simple
+		/*for(int i=0;i<x.numRows;i++)
+		{
+			double xx=x.get(i, 0);
+			y.set(i, 0, param.get(0, 0)*xx*xx+param.get(1,0)*xx+param.get(2,0));			
+		}*/		
+		double pixelX;
+		byte[] pixelResult=new byte[3];		
+		int[] pos=new int[3];
+		double[] E=new double[3];
+		double[] L=new double[3];
+		double D2;			
+		double[] H=new double[3];
+		SimpleMatrix R;
+		double[] Hn=new double[3];
+		double[] Hnp=new double[3];
+		SimpleMatrix S,M;
+		double[] HnpW=new double[3];
+		byte[] alb_d=new byte[3];
+		byte[] alb_s=new byte[3];
+		byte[] specv=new byte[]{1,1,(byte)200};
+		//double alpha=1;//lobe speculaire => plus grand plus retrecie et concentré
+		//double intensity=0.3;//varie 0 à 1
+		double rod=param.get(0,0);
+		double ros=param.get(1, 0);
+		double s1=param.get(2,0);
+		double s2=param.get(3,0);
+		double s3=param.get(4,0);
+		double nx=param.get(5,0);
+		double ny=param.get(6,0);
+		double nz=1;		
+		double tof=param.get(7,0);		
+		double alpha=param.get(8, 0);		
+		//contrainte paramètres
+		/*
+		rod=Math.exp(rod);
+		ros=Math.exp(ros);
+		s1+=0.001;
+		s2+=0.001;
+		double[] N=MappingN(nx, ny);		
+		tof=Math.exp(tof)+0.3;
+		alpha=Math.exp(alpha)+0.5;*/
+		//double[] N=MappingN(nx, ny);
+		try
+		{			
+			compte++;
+			for(int i=0;i<x.numRows;i++)
+			{
+				double xx=x.get(i,0);
+				pixelX=oneChannelMData.get(i, 0);//it will contain a pixel value. And it depends of the value of which channel
+				pos=decodePosition(xx);
+				//double[] newPos=normalize(pos);
+				//E=normalize(cam-pos)
+				E=normalize(XY(pos,cam));
+				L=calculeL(pos,lum);
+				double[] le=addXY(L,E);
+				H=normalize(le);
+				double[] N=normalize(new int[]{pos[1],pos[0],2});//on a changé N par Nx et Ny en tant que paramètre de L-M => step2_1104_soir
+				D2=dot(L,L);
+				R=new SimpleMatrix(new double[][]{
+					{0,0,N[0]},
+					{0,0,N[1]},
+					{-N[0],-N[1],0}
+				});
+				Hn=normalize(calculeHn(H,N,R));	
+				Hnp=normalize(div(Hn,Hn[2]));
+				M=new SimpleMatrix(new double[][]{
+					{lightColor.getRed(),lightColor.getBlue()},
+					{lightColor.getBlue(),lightColor.getGreen()},
+				});
+				
+				HnpW=normalize(calculeHnpW(M,Hnp[0],Hnp[1]));
+				double spec=Math.exp(-Math.pow(dot(new double[]{HnpW[0],HnpW[1]},new double[]{Hnp[0],Hnp[1]}), alpha*0.5));
+				double cosine=Math.max(0, dot(N,L));
+				double F0=0.04;
+				double fres=F0+(1-F0)*Math.pow(1.0-Math.max(0, dot(H,E)), 5.0);
+				spec=spec*fres/F0;
+				//sqrt is necessary to "rough gamma" => I don't understand it but if we omit the sqrt the colors are not correct
+				//double v=((spec*ros+rod)*cosine/D2*0.5*255);//step2_1104
+				//double v=((spec*pixelX+pixelX)*cosine/D2*0.5*100);//step2_1104_x
+				double v=((spec*pixelX+pixelX)*cosine/D2*0.2*pixelX);//on va commenter tous les paramètres mapping => step2_1104_soir
+				v=Math.sqrt(v);
+				double value=v>256?255:v<0?0:v;
+				y.set(i, 0,value);//intensity=0.5
+				/*
+				//modele BRDF A
+				//E=normalize(cam-pos) => EP
+				E=normalize(XY(pos,cam));				
+				L=calculeL(pos,lum);
+				double[] le=addXY(L,E);
+				H=normalize(le);
+				//N=normalize(new int[]{pos[1],pos[0],2});
+				D2=dot(L,L);
+				R=new SimpleMatrix(new double[][]{
+					{0,0,N[0]},
+					{0,0,N[1]},
+					{-N[0],-N[1],0}
+				});
+				Hn=calculeHn(H,N,R);// Halfway vector in normal-oriented coordinates (so normal is [0,0,1])
+				Hnp=div(Hn,Hn[2]);//h=hxy/hz
+				S=new SimpleMatrix(new double[][]{
+					{s1,s3},
+					{s3,s2},
+				});
+				HnpW=calculeHnpW(S,Hnp[0],Hnp[1]);
+				double spec=Math.exp(-Math.pow(dot(new double[]{HnpW[0],HnpW[1]},new double[]{Hnp[0],Hnp[1]}), (int)Math.round(alpha*0.5)));//D(h)
+				if(Double.isInfinite(spec) ||Double.isNaN(spec))spec=1;
+				double vp=v_p(newPos, tof);if(Double.isInfinite(vp) || Double.isNaN(vp))vp=1;
+				double IP=1/(E[0]*E[0]+E[1]*E[1]+E[2]*E[2]);if(Double.isInfinite(IP) || Double.isNaN(IP))IP=1;
+				double CP=Math.max(0, dot(N,E));
+				double somme=vp*IP*CP;
+				if(somme<0)somme*=-1;
+				double value=somme*(Math.abs(rod+ros*spec*pixelX));if(Double.isInfinite(value) || Double.isNaN(value))value=pixelX;
+				y.set(i, 0,value);
+				//double cosine=Math.max(0, dot(N,L));
+				//double F0=0.04;
+				//double fres=F0+(1-F0)*Math.pow(1.0-Math.max(0, dot(H,E)), 5.0);
+				//double fres=F0+(1-F0)*Math.pow(1.0-Math.max(0, dot(H,E)), fresnel_pow);
+				//spec=spec*fres/F0;
+				//sqrt is necessary to "rough gamma" => I don't understand it but if we omit the sqrt the colors are not correct
+				//y.set(i, 0,Math.sqrt(((spec*pixelX+pixelX)*cosine/D2*intensity*255)));//fichier step2_0704
+				//y.set(i, 0,Math.sqrt(((spec*lightColor+lightColor)*cosine/D2*intensity*255)));//fichier step2_0804 last test
+				*/
+				//
+				/*
+				//modele Lafortune => on va travailler sur le répère de l'image
+				P=decodePosition(xx);
+				//P=ChangeBase(p);
+				EP=vectXY(cam, P);
+				EPn=normalize(EP);				
+				LP=vectXY(lum,P);
+				LPn=normalize(LP);				
+				lobe=Cxi*(EPn[0]*LPn[0]+EPn[1]*EPn[1])+Czi*(EPn[2]*LPn[2]);
+				value=rod*lightColor+Math.pow(lobe<0?0:lobe, N)*100;				
+				y.set(i,0,value);*/
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		Y=y;		
+	}
+	public Fonction(int x,int y,Color light)
+	{
+		cam=new int[]{0,y,5};
+		lum=new int[]{x,y,10};
+		lightColor=light;
+		//cam=ChangeBase(new int[]{0,0,1});
+		//lum=ChangeBase(new int[]{0,0,1});//=> fichier optimize2 avec une lissage sur tous les tiles avant de optimisation et optimize3 sans lissage
+		//lum=ChangeBase(new int[]{100,-50,1});//=> fichier optimize0
+		//lum=new int[]{3000,500,1}; //=> fichier optimize1 + optimize8
+	}
+	public void getPositionList(HashMap<Integer,int[]>pos)
+	{
+		p=pos;
+	}
+	//Changement de base vers le répère de l'image	
+	public static int[] ChangeBase(int[] xyz)
+    {
+        int[] P = new int[3];
+        int[] u = new int[] { 1, 0, 0 };
+        int[] v = new int[] { 0, 1, 0 };
+        int[] w = new int[] { 0, 0, 1 };
+        P[0] = u[0] * xyz[0]+(3264/2) ;
+        P[1] = v[1] * xyz[1] +(2304/2);
+        P[2] = w[2]*xyz[2];
+        return P;
+    }
+	public static int[] decodePosition(double pos)
+	{
+		int[] p=new int[3];
+		String[] pattern=Double.toString(pos).split("\\.");
+		p[0]=Integer.parseInt(pattern[0]);
+		p[1]=Integer.parseInt(pattern[1].substring(0, pattern[1].length()-1));
+		p[2]=0;
+		return p;
+	}
+	/*
+	 * This is used for BRDF model A
+	 */
+	public static int[] XY(int[] x,int[] y)
+	{
+		return new int[]{y[0]-x[0],y[1]-x[1],y[2]-x[2]};
+	}
+	public static double[] normalize(int[] x)
+	{
+		double norm=Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
+		if(norm==0)return new double[3];
+		return new double[]{x[0]/norm,x[1]/norm,x[2]/norm};
+	}
+	public static double[] normalize(double[] x)
+	{
+		double norm=Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
+		return new double[]{x[0]/norm,x[1]/norm,x[2]/norm};
+	}
+	public static double dot(double[] x,double[] y)
+	{
+		if(x.length==2)return x[0]*y[0]+x[1]*y[1];
+		return x[0]*y[0]+x[1]*y[1]+x[2]*y[2];
+	}
+	public static double dot(int[] x,int[] y)
+	{
+		return x[0]*y[0]+x[1]*y[1]+x[2]*y[2];
+	}
+	public static double[] calculeL(int[]x,int[]y)
+	{
+		int[] xy=XY(x,y);
+		return normalize(xy);
+	}
+	public static double[] addXY(double[]x,double[] y)
+	{
+		return new double[]{x[0]+y[0],x[1]+y[1],x[2]+y[2]};
+	}
+	public static double[] calculeHn(double[] h,double[] n,SimpleMatrix r)
+	{		
+		SimpleMatrix ha=new SimpleMatrix(new double[][]{{h[0]},{h[1]},{h[2]}});
+		SimpleMatrix first=ha.plus(r.mult(ha)).plus(1);
+		SimpleMatrix second=r.mult(r.mult(ha)).mult(new SimpleMatrix(new double[][]{{n[2]+1}}));
+		return first.elementDiv(second).getMatrix().data;		
+	}
+	public static double[] div(double[] xy,double e)
+	{
+		return new double[]{xy[0]/e,xy[1]/e,xy[2]/e};
+	}
+	public static double[] calculeHnpW(SimpleMatrix m,double x,double y)
+	{
+		SimpleMatrix xy=new SimpleMatrix(new double[][]{
+			{x},
+			{y}
+		});
+		SimpleMatrix temp=m.mult(xy);
+		double[] res=temp.getMatrix().data;
+		return new double[]{res[0],res[1],1};
+	}
+	public static int byteColorCVtoIntJava(byte b)
+	{		
+		int i=(b+128)+128;		
+		return b>=0?(int)b:i;
+	}
+	public static double[] MappingN(double nx,double ny)
+	{
+		return new double[]{
+			nx/Math.sqrt((nx*nx+ny*ny)+1),
+			ny/Math.sqrt((nx*nx+ny*ny)+1),
+			1/Math.sqrt((nx*nx+ny*ny)+1)
+		};
+	}
+	public static double v_p(double[] p,double tof)
+	{		
+		return Math.exp(-(p[0]*p[0]+p[1]*p[1])/(tof*tof));
+	}
+	/*
+	 * This
+	 * is used for Lafortune model
+	 */
+	/*
+	private double v_p(int[] p,double tof)
+	{
+		double norm_p=normeX(p);
+		return Math.exp(-norm_p/(tof*tof));
+	}
+	private double c_P(int[] n,int[] EP)
+	{
+		return Math.max(0, dot(n,EP));
+	}
+	public static int[] vectXY(int[] x,int[] y)
+	{
+		int[] xy=new int[3];
+		xy[0]=y[0]-x[0];
+		xy[1]=y[1]-x[1];
+		xy[2]=y[2]-x[2];
+		return xy;
+	}
+	
+	public static double normeX(int[]x)
+	{
+		return x[0]*x[0]+x[1]*x[1]+x[2]*x[2];
+	}
+	private double dot(int[] x,int[] y)
+	{
+		return x[0]*y[0]+x[1]*y[1]+x[2]*y[2];
+	}
+	private double dot(double[] x,double[] y)
+	{
+		return x[0]*y[0]+x[1]*y[1]+x[2]*y[2];
+	}
+	public static double[] normalize(int[] x)
+	{
+		double mod=Math.sqrt(normeX(x));
+		double[] y=new double[3];
+		y[0]=((double)x[0])/mod;
+		y[1]=((double)x[1])/mod;
+		y[2]=((double)x[2])/mod;
+		return y;
+	}*/
+}
---
+++ src/Main.java
@@ -0,0 +1,377 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import org.ejml.data.DenseMatrix64F;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+public class Main 
+{	
+	public static void main(String[] args)
+	{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		try
+		{
+			String dir="C:\\Users\\ralambomahay1\\Downloads\\stage\\twoshot_data_input\\book_leather_red\\";//"C:\\Users\\ralambomahay1\\Downloads\\Java_workspace\\newGit\\Data\\";//
+			String path="source_tile_relit_";
+			//BufferedImage[][] tiles=new BufferedImage[12][16];
+			int tileWNumber=17;
+			int tileHNumber=12;
+			int brdfEstimation=tileWNumber*tileHNumber;
+			BufferedImage[][] tiles=new BufferedImage[tileHNumber][tileWNumber];
+				
+			Color color;
+			int c,b,g,r;			
+			
+			List<double[]> blueData=new ArrayList<>();
+			List<double[]> greenData=new ArrayList<>();
+			List<double[]> redData=new ArrayList<>();	
+			HashMap<Integer, int[]> p=new HashMap<>();
+			double[][] posData=new double[192*192][];//it is used only for test			
+			List<DenseMatrix64F> posDataMatrix=new ArrayList<>();//it contains 192*192 DenseMatrix64F. Each item refers to position array in world coordinates
+			List<DenseMatrix64F> paramBList=new ArrayList<>();//it contains all parameters for each pixel
+			List<DenseMatrix64F> paramGList=new ArrayList<>();//it contains all parameters for each pixel
+			List<DenseMatrix64F> paramRList=new ArrayList<>();//it contains all parameters for each pixel
+			
+			int colonne=192;
+			int ligne=192;
+			int pI=0;
+			List<DenseMatrix64F> blueMData=new ArrayList<>();			
+			List<DenseMatrix64F> greenMData=new ArrayList<>();
+			List<DenseMatrix64F> redMData=new ArrayList<>();
+			List<DenseMatrix64F> blueMDataAdjusted=new ArrayList<>();
+			List<DenseMatrix64F> greenMDataAdjusted=new ArrayList<>();
+			List<DenseMatrix64F> redMDataAdjusted=new ArrayList<>();
+			
+			System.out.println("start intialisation des données...");
+			Mat tempMat;
+			BufferedImage tempBuffer;
+			for(int fileI=0;fileI<tileHNumber;fileI++)
+			{							
+				for(int fileJ=0;fileJ<tileWNumber;fileJ++)
+				{									
+					tempBuffer=ImageIO.read(new File(dir+path+fileI+"_"+fileJ+".jpg"));
+					
+					//on va lisser chaque tiles avant d'appliquer le svbrdf optimisation
+					tempMat=convertTileToCV(tempBuffer);
+					Imgproc.GaussianBlur(tempMat, tempMat, new Size(3,3), 1);
+					tempBuffer=convertCVtoJava(tempMat);
+					tiles[fileI][fileJ]=tempBuffer;
+				}						
+			}
+			System.out.println("Fin lecture files");
+			for(int i=0;i<192;i++)
+			{
+				for(int j=0;j<192;j++)
+				{
+					double[] blue=new double[brdfEstimation];
+					double[] green=new double[brdfEstimation];
+					double[] red=new double[brdfEstimation];
+					double[] temp=new double[brdfEstimation];
+					DenseMatrix64F tempPos=new DenseMatrix64F(brdfEstimation,1);
+					DenseMatrix64F tempB=new DenseMatrix64F(brdfEstimation,1);
+					DenseMatrix64F tempG=new DenseMatrix64F(brdfEstimation,1);
+					DenseMatrix64F tempR=new DenseMatrix64F(brdfEstimation,1);
+					int iter=0;
+					for(int tileI=0;tileI<tileHNumber;tileI++)
+					{						
+						for(int tileJ=0;tileJ<tileWNumber;tileJ++)
+						{
+							c=tiles[tileI][tileJ].getRGB(j, i);
+							color=new Color(c);
+							b=color.getBlue();
+							g=color.getGreen();
+							r=color.getRed();
+							blue[iter]=b;
+							green[iter]=g;
+							red[iter]=r;
+							//stocke position
+							String pp=((tileI*ligne)+i)+"."+((tileJ*colonne)+j+"1");
+							temp[iter]=Double.parseDouble(pp);
+							iter++;			
+						}
+					}
+					posData[pI]=temp;
+					tempPos.data=temp;
+					posDataMatrix.add(tempPos);
+					pI++;
+					blueData.add(blue);
+					greenData.add(green);
+					redData.add(red);
+					tempB.data=blue;					
+					tempG.data=green;
+					tempR.data=red;
+					blueMData.add(tempB);
+					greenMData.add(tempG);
+					redMData.add(tempR);					
+				}
+			}
+			//String[] kk=Double.toString(posDataMatrix.get(0).get(1)).split("\\.");
+			//System.err.println(kk[1].substring(0, kk[1].length()-1));
+			//for(int i=0;i<192;i++)System.err.println(posDataMatrix.get(0).get(i)+" -- "+posData[0][i]);
+			//for(int i=0;i<192;i++)System.out.println(blueMData.get(0).get(i, 0)+" -- "+greenMData.get(0).get(i, 0)+" -- "+redMData.get(0).get(i, 0));
+			
+			
+			//
+			System.out.println("fin initialisation des données.");
+			//System.out.println(posData.length+"/"+greenMData.size()+"/"+redMData.size());
+			Date dt=new Date();
+			String now=dt.toString();
+			System.out.println("Debut d'optimisation pour chaque pixel de chaque tile.. "+now);
+			//init parameters value for all optimization
+			Color av=AverageColor();
+			/*//Lafortune model optimization parameters
+			DenseMatrix64F paramB=new DenseMatrix64F(new double[][]{				
+				{((double)av.getBlue())/255},
+				{1},
+				{1},
+				{3}
+			});
+			DenseMatrix64F paramG=new DenseMatrix64F(new double[][]{
+				{((double)av.getGreen())/255},//rod				
+				{1},
+				{1},
+				{3}
+			});
+			DenseMatrix64F paramR=new DenseMatrix64F(new double[][]{
+				{((double)av.getRed())/255},//rod				
+				{1},
+				{1},
+				{3}
+			});*/
+			DenseMatrix64F paramB=new DenseMatrix64F(new double[][]{
+				{av.getBlue()},//rod
+				{-1},//ros
+				{-3},//s1
+				{-3},//s2
+				{0},//s3
+				{10},//nx
+				{10},//ny => nz = 1 
+				{-1},//tof
+				{0.4},//alpha				
+			});
+			DenseMatrix64F paramG=new DenseMatrix64F(new double[][]{
+				{av.getGreen()},//rod
+				{0},//ros
+				{-3},//s1
+				{-3},//s2
+				{0},//s3
+				{10},//nx
+				{10},//ny => nz = 1 
+				{-1},//tof
+				{0.4},//alpha	
+			});
+			DenseMatrix64F paramR=new DenseMatrix64F(new double[][]{
+				{av.getBlue()},//rod
+				{0},//ros
+				{-3},//s1
+				{-3},//s2
+				{0},//s3
+				{10},//nx
+				{10},//ny => nz = 1 
+				{-1},//tof
+				{0.4},//alpha	
+			});
+			try
+			{
+				for(int i=0;i<36864;i++)
+				{
+					//il faut optimiser chaque pixel
+					Fonction f=new Fonction(17*192,12*192,av);
+					Fonction f2=new Fonction(17*192,12*192,av);
+					Fonction f3=new Fonction(17*192,12*192,av);
+					f.oneChannelMData=blueMData.get(i);//f.lightColor=0;//optimize10 file
+					f2.oneChannelMData=greenMData.get(i);//f2.lightColor=255;
+					f3.oneChannelMData=redMData.get(i);//f3.lightColor=0;
+					LevenbergMarquardt lm=new LevenbergMarquardt(f);
+					LevenbergMarquardt lm2=new LevenbergMarquardt(f2);
+					LevenbergMarquardt lm3=new LevenbergMarquardt(f3);
+					DenseMatrix64F Yb=blueMData.get(i);
+					DenseMatrix64F Yg=greenMData.get(i);
+					DenseMatrix64F Yr=redMData.get(i);	
+					//optimize5 (0,0,50);optimize6 (0,0,255)
+					//blue
+					lm.optimize(paramB, posDataMatrix.get(i), Yb);
+					paramBList.add(lm.getParameters());
+					//System.err.println(f.compte);f.compte=0;
+					blueMDataAdjusted.add(f.Y);	
+					//green								
+					lm2.optimize(paramG, posDataMatrix.get(i), Yg);
+					paramGList.add(lm2.getParameters());
+					//System.err.println(f.compte);f.compte=0;
+					greenMDataAdjusted.add(f2.Y);	
+					//Red											
+					lm3.optimize(paramR, posDataMatrix.get(i), Yr);
+					paramRList.add(lm3.getParameters());
+					//System.err.println(f.compte);f.compte=0;
+					redMDataAdjusted.add(f3.Y);
+					System.out.println("pixel:"+i+" optimisé");	
+					//lm.getParameters().print();
+					//lm2.getParameters().print();
+					//lm3.getParameters().print();	
+				}
+			}
+			catch(Exception ee)
+			{
+				System.err.println("Boucle optimize erreur!"+ee.getMessage());
+			}
+			dt=new Date();
+			System.out.println("Fin de l'optimisation."+now+" à "+dt.toString());
+			System.err.println("Display:");
+			//for(int i=0;i<192;i++)System.out.println(blueMData.get(0).get(i, 0)+" -- "+blueMDataAdjusted.get(0).get(i)+"/"+greenMData.get(0).get(i, 0)+" -- "+greenMDataAdjusted.get(0).get(i)+"/"+redMData.get(0).get(i, 0)+" -- "+redMDataAdjusted.get(0).get(i));
+			
+			
+			 //On peut faire la reconstruction dans cette partie ou optimisation
+			BufferedImage image=new BufferedImage(tileWNumber*192,tileHNumber*192,BufferedImage.TYPE_3BYTE_BGR);
+			int[] position;
+			DenseMatrix64F temp;
+			DenseMatrix64F tempR;
+			DenseMatrix64F tempG;
+			DenseMatrix64F tempB;
+			
+			for(int i=0;i<192*192;i++)
+			{
+				temp=posDataMatrix.get(i);
+				tempR=redMDataAdjusted.get(i);
+				//tempR=redMData.get(i);
+				tempG=greenMDataAdjusted.get(i);
+				//tempG=greenMData.get(i);
+				tempB=blueMDataAdjusted.get(i);
+				//tempB=blueMData.get(i);
+				//paramB=paramBList.get(i);
+				//paramG=paramGList.get(i);
+				//paramR=paramRList.get(i);
+				
+				for(int j=0;j<brdfEstimation;j++)
+				{
+					position=Fonction.decodePosition(temp.get(j,0));
+					//si <0 => valeur absoulue
+					double rr=Math.abs(tempR.get(j, 0));
+					double gg=Math.abs(tempG.get(j, 0));
+					double bb=Math.abs(tempB.get(j, 0));
+					//resultat fichier optimize4
+					/*r=(rr>256)?255:(int)rr;
+					g=(gg>256)?255:(int)gg;
+					b=(bb>256)?255:(int)bb;*/
+					r=(tempR.get(j, 0)>256)?255:( tempR.get(j,0)<0?0: (int)tempR.get(j,0));
+					g=(tempG.get(j, 0)>256)?255:( tempG.get(j,0)<0?0: (int)tempG.get(j,0));
+					b=(tempB.get(j, 0)>256)?255:( tempB.get(j,0)<0?0: (int)tempB.get(j,0));
+					/*//recompute brdf
+					int[] EP=Fonction.vectXY(Fonction.ChangeBase(new int[]{0,0,1}), position);
+					double[] EPn=Fonction.normalize(EP);				
+					int[] LP=Fonction.vectXY(Fonction.ChangeBase(new int[]{100,-50,1}),position);
+					double[] LPn=Fonction.normalize(LP);				
+					double lobe=paramB.get(1, 0)*(EPn[0]*LPn[0]+EPn[1]*EPn[1])+paramB.get(2,0)*(EPn[2]*LPn[2]);
+					b=(int) (paramB.get(0,0)*tempB.get(j, 0)+Math.pow(lobe<0?0:lobe, paramB.get(3,0))*tempB.get(j, 0));	
+					lobe=paramG.get(1, 0)*(EPn[0]*LPn[0]+EPn[1]*EPn[1])+paramG.get(2,0)*(EPn[2]*LPn[2]);
+					g=(int) (paramG.get(0,0)*tempG.get(j, 0)+Math.pow(lobe<0?0:lobe, paramG.get(3,0))*tempG.get(j, 0));
+					lobe=paramR.get(1, 0)*(EPn[0]*LPn[0]+EPn[1]*EPn[1])+paramR.get(2,0)*(EPn[2]*LPn[2]);
+					r=(int) (paramR.get(0,0)*tempR.get(j, 0)+Math.pow(lobe<0?0:lobe, paramR.get(3,0))*tempR.get(j, 0));
+					r=r>256?255:r;
+					g=g>256?255:g;					
+					b=b>256?255:b;*/
+					color=new Color(r,g,b);					
+					image.setRGB(position[1],position[0], color.getRGB());
+				}
+			}
+			System.out.println("Debut enregistremen");
+			saveImage(image, "C:\\Users\\ralambomahay1\\Downloads\\Java_workspace\\newGit\\Data\\step2.jpg");
+		}
+		catch(Exception e)
+		{
+			System.err.println("Erreur general:"+e.getMessage());
+		}		
+		/*Fonction fonc=new Fonction();
+		LevenbergMarquardt lm=new LevenbergMarquardt(fonc);
+		//Seed inital parameters
+		lm.optimize(new DenseMatrix64F(new double[][]{{0},{0},{0}}), new DenseMatrix64F(new double[][]{{0.2059},{0.4412},{0.7353},{0.9874},{1.1975}}), new DenseMatrix64F(new double[][]{{-1.0966},{0.4916},{1.4076},{1.0630},{0.2059}}));		
+		DenseMatrix64F f=lm.getParameters();
+		f.print();		
+		fonc.Y.print();*/
+	}
+	public static int byteColorCVtoIntJava(byte b)
+	{		
+		int i=(b+128)+128;		
+		return b>=0?(int)b:i;
+	}
+	public static double Gaussian(Random rd,double mu,double std)
+	{
+		//variance est déjà au carré			
+		return rd.nextGaussian()*std+mu;
+	}
+	public static Color AverageColor()
+	{
+		Color c=null;
+		int temp;
+		int tempR=0;
+		int tempG=0;
+		int tempB=0;
+		try 
+		{
+			BufferedImage image=ImageIO.read(new File("C:\\Users\\ralambomahay1\\Downloads\\stage\\twoshot_data_input\\book_leather_red\\flash.png"));
+			//BufferedImage image=ImageIO.read(new File("C:\\Users\\ralambomahay1\\Downloads\\Java_workspace\\newGit\\Data\\book_black_input_flash.jpg"));
+			for(int i=0;i<image.getHeight();i++)
+			{
+				for(int j=0;j<image.getWidth();j++)
+				{
+					temp=image.getRGB(j, i);
+					c=new Color(temp);
+					tempR+=c.getRed();
+					tempG+=c.getGreen();
+					tempB+=c.getBlue();
+				}
+			}
+			tempR/=image.getHeight()*image.getWidth();
+			tempG/=image.getHeight()*image.getWidth();
+			tempB/=image.getHeight()*image.getWidth();
+			c=new Color(tempR,tempG,tempB);
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			System.out.println("AverageColor erreur:"+e.getMessage());
+		}
+		return c;
+	}
+	public static void saveImage(BufferedImage image,String path)
+	{
+		try
+		{			
+			//save image		
+			ImageIO.write(image, "jpg", new File(path));			
+		}
+		catch(IOException e)
+		{
+			System.err.println("Erreur lors de l'enregistrement:"+e.getMessage());
+		}		
+	}
+	public static Mat convertTileToCV(BufferedImage im)
+	{
+		Mat m=new Mat(im.getHeight(), im.getWidth(), CvType.CV_8UC3);
+		byte[] data=((DataBufferByte)im.getRaster().getDataBuffer()).getData();
+		m.put(0, 0, data);
+		return m;
+	}
+	public static BufferedImage convertCVtoJava(Mat mat)
+	{
+		BufferedImage image=new BufferedImage(mat.cols(), mat.rows(),BufferedImage.TYPE_3BYTE_BGR);
+		byte[] data=new byte[mat.rows()*mat.cols()*mat.channels()];
+		mat.get(0, 0,data);
+		byte[] dest=((DataBufferByte)image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(data, 0, dest, 0, mat.rows()*mat.cols()*mat.channels());		
+		return image;
+	}
+}
